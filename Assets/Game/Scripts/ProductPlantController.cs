5a2dc0fd11a07d056bd3b8cc65b9d0adc4fab99d
cagataysoyer <cagatay@stardust.gs>
2024-04-04 18:06:52 +0300

    add: dotween update: player can harvest products

--- Assets/Game/Scripts/ProductPlantController.cs
+++ Assets/Game/Scripts/ProductPlantController.cs
@@ -1,14 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ProductPlantController : MonoBehaviour
 {
+    private bool isReadyToHarvest;
+    private Vector3 originalScale;
+
+    private void Awake() 
+    {
+        originalScale = transform.localScale;
+        isReadyToHarvest = true;    
+    }
     private void OnTriggerEnter(Collider other) 
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && isReadyToHarvest)
         {
+            StartCoroutine(HarvestProduct());
             Debug.Log("Player Entered");
-        }    
+        }
+    }
+    IEnumerator HarvestProduct()
+    {
+        isReadyToHarvest = false;
+
+        Vector3 targetScale = originalScale / 3;
+        transform.DOScale(targetScale, 1f);
+
+        yield return new WaitForSeconds(3f);
+        
+        isReadyToHarvest = true;
+        transform.DOScale(originalScale, 1f).SetEase(Ease.OutBack);
     }
 }
